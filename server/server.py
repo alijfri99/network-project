574c6706c953967a66b7e689bd80be2e7a1eb27d
Ali <alijfri99@gmail.com>
2020-06-15 01:29:38 +0430

    Implemented a basic UDP server

--- server/server.py
+++ server/server.py
@@ -1,1 +1,13 @@
-print("Server Started")
+import socket
+
+server = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+try:
+    server.bind(('127.0.0.1',8080))
+    print("Server Started")
+except:
+    print("Error: Could not start the server")
+
+while(True):
+    data, addr = server.recvfrom(4096)
+    print(data.decode("utf-8"))
+    print(addr)
